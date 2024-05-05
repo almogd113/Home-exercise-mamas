a9782f5ecc8e77b7d59737c9e9ff13df406e5d52
אלמוג דהרי <almogd24@gmail.com>
2024-05-05 19:41:33 +0300

    created node class

---
+++ node_exercise/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace node_exercise
+{
+    public class Node
+    {
+        public int Value{ get; set; }
+        public Node Next{ get; set; }
+
+    }
+}

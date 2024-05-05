e5cc279b42897462fdc841a6e870fe2691130845
אלמוג דהרי <almogd24@gmail.com>
2024-05-06 01:11:24 +0300

    added sort function - implement of merge sort. added max and min node.

--- NodeExercise/LinkedList.cs
+++ NodeExercise/LinkedList.cs
@@ -1,41 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NodeExercise
 {
-    class LinkedList<T>
+    class LinkedList<T> : IEnumerable<int>
     {
-        public Node<T> head; // pointer to first node
-        private Node<T> last; // pointer to the last node
-
-        public LinkedList(Node<T> node)
+        public Node<int> head; // pointer to first node
+        public Node<int> last; // pointer to the last node
+        public LinkedList(Node<int> head)
         {
-            head = node;
-            last = head;
+            this.head = head;
+            last = this.head;
         }
 
-        public void Append(T value)
+        public void Append(int value)
         {
-            Node<T> node = new Node<T>(value);
+
+            Node<int> node = new Node<int>(value);
             last.Next = node;
-            last = last.Next;   
+            last = last.Next;
+
+
         }
 
-        public void Prepend(T value)
+        public void Prepend(int value)
         {
-            Node<T> node = new Node<T>(value);
+            Node<int> node = new Node<int>(value);
             node.Next = head;
             head = node;
         }
 
-        public T Pop()
+        public int Pop()
         {
-            T nodeValue = last.Value;
+            int nodeValue = last.Value;
 
             // run over the list till one before last
-            Node<T> pointer = head;
-            while(pointer.Next.Next != null)
+            Node<int> pointer = head;
+            while (pointer.Next.Next != null)
             {
                 pointer = pointer.Next;
             }
@@ -45,16 +48,157 @@ namespace NodeExercise
 
         }
 
-        public T Unqueue()
+        public int Unqueue()
         {
-            T value = head.Value;
+            int value = head.Value;
             head = head.Next;
             return value;
         }
 
+        public bool IsCircular()
+        {
+            return last.Next != null;
+        }
 
+        Node<int> SortedMerge(Node<int> node1, Node<int> node2)
+        {
+            Node<int> result = null;
+            // check if one of the nodes are null - return the other one
+            if (node1 == null)
+                return node2;
+            if (node2 == null)
+                return node1;
 
+            // get the smaller node and recursively define the next node as the minimum node in the checked sequence
+            if (node1.Value <= node2.Value)
+            {
+                result = node1;
+                result.Next = SortedMerge(node1.Next, node2);
+            }
+            else
+            {
+                result = node2;
+                result.Next = SortedMerge(node1, node2.Next);
+            }
+            return result;
+        }
 
+        public Node<int> MergeSort(Node<int> head)
+        {
+            // check if head is null
+            if (head == null || head.Next == null)
+            {
+                return head;
+            }
 
+            // get the middle of the list
+            Node<int> middle = GetMiddle(head);
+            Node<int> nextofmiddle = middle.Next;
+
+            // set the next of middle node to null
+            middle.Next = null;
+
+            // Apply mergeSort on left list
+            Node<int> left = MergeSort(head);
+
+            // Apply mergeSort on right list
+            Node<int> right = MergeSort(nextofmiddle);
+
+            // Merge the left and right lists
+            Node<int> sortedlist = SortedMerge(left, right);
+            return sortedlist;
+        }
+
+        //get the middle of the linked list
+        Node<int> GetMiddle(Node<int> head)
+        {
+            // Base case
+            if (head == null)
+                return head;
+            Node<int> currentPointer = head.Next;
+            Node<int> previousPointer = head;
+
+            // Move currentPointer by two and previous pointer by one
+            // At the end previous pointer will point to middle node
+            while (currentPointer != null)
+            {
+                currentPointer = currentPointer.Next;
+                if (currentPointer != null)
+                {
+                    previousPointer = previousPointer.Next;
+                    currentPointer = currentPointer.Next;
+                }
+            }
+            return previousPointer;
+        }
+
+        private void set_last_node()
+        {
+            Node<int> pointer = head;
+            while (pointer.Next != null)
+            {
+                pointer = pointer.Next;
+            }
+            last = pointer;
+        }
+        public void Sort()
+        {
+            head = this.MergeSort(head);
+            this.set_last_node();
+        }
+
+        public Node<int> GetMaxNode()
+        {
+            this.Sort();
+            return last;
+        }
+
+        public Node<int> GetMinNode()
+        {
+            this.Sort();
+            return head;
+        }
+        //public IEnumerable<int> ToList()
+        //{
+        //    return GetEnumerator();
+        //}
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            Node<int> current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        // }
+
+        // IEnumerator IEnumerable.GetEnumerator()
+        // {
+        //     return this.ToList()
+        // }
+
+        //public LinkedList<int> GetEnumerator()
+        //{
+        //    return new LinkedList<int>();
+        //}
+
+        // Must also implement IEnumerable.GetEnumerator, but implement as a private method.
+        //private IEnumerator GetEnumerator1()
+        //{
+        //    yield return this.GetEnumerator();
+        //}
+
+        //IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        //{
+        //    return (IEnumerator<int>)GetEnumerator1();
+        //}
     }
 }

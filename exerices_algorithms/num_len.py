51e3ba76b5662eb3e3141d8befe98d8ce069744a
אלמוג דהרי <almogd24@gmail.com>
2024-05-05 12:27:57 +0300

    num len code - works

---
+++ exerices_algorithms/num_len.py
@@ -0,0 +1,14 @@
+import math
+
+
+def num_len(number: int) -> int:
+    # log10 gives as the result of the number of
+    # digits we have in each number besides the last digit
+    # for example every number that is smaller than 10 would give as a result of 0
+    # because there is no presumption where base 10 can be a single-digit number
+    # that's why I added + 1
+
+    return int(math.log10(number) + 1)
+
+
+print(num_len(1))

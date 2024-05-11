649b115adf6bb774e1ec34ee65267302a85a8019
אלמוג דהרי <almogd24@gmail.com>
2024-05-11 13:06:15 +0300

    added enums files. direction and game status

---
+++ 2048/Game2048/logic/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048.logic
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+}
---
+++ 2048/Game2048/logic/GameStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048.logic
+{
+    public enum GameStatus
+    {
+        Win,
+        Lose,
+        Idle
+    }
+}
